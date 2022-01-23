a2cf35fa7598b39d151c061ff08e9c5a743c7c8e
ChaoticEvan <evan.voordeckers@yahoo.com>
2022-01-23 13:30:28 -0700

    Added logging

--- DoorbellDinger/DoorbellDinger/Program.cs
+++ DoorbellDinger/DoorbellDinger/Program.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
-using System;
-using System.Configuration;
+﻿using System;
+using Microsoft.Extensions.Configuration;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 
@@ -10,25 +9,40 @@ namespace DoorbellDinger
     {
         static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
+            try
+            {
+                Console.WriteLine("INFO: Getting configuration values");
+                var config = BuildConfig();
+
+                var accountSid = config.GetSection("TwilioSid").Value;
+                var accountAuthToken = config.GetSection("TwilioAuthToken").Value;
+                var fromNumber = config.GetSection("FromNumber").Value;
+                var toNumber = config.GetSection("ToNumber").Value;
+
+                Console.WriteLine("INFO: Building TwilioClient");
+                TwilioClient.Init(accountSid, accountAuthToken);
+
+                Console.WriteLine("INFO: Sending text message");
+                MessageResource.Create(
+                    body: "Does this work?",
+                    from: new Twilio.Types.PhoneNumber(fromNumber),
+                    to: new Twilio.Types.PhoneNumber(toNumber)
+                );
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("ERROR: Error occured");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        private static IConfigurationRoot BuildConfig()
+        {
+            return new ConfigurationBuilder()
                 .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
                 .AddUserSecrets<Program>()
                 .Build();
-
-            var accountSid = config.GetSection("TwilioSid").Value;
-            var accountAuthToken = config.GetSection("TwilioAuthToken").Value;
-            var fromNumber = config.GetSection("FromNumber").Value;
-            var toNumber = config.GetSection("ToNumber").Value;
-
-            TwilioClient.Init(accountSid, accountAuthToken);
-
-            var message = MessageResource.Create(
-                body: "Does this work?",
-                from: new Twilio.Types.PhoneNumber(fromNumber),
-                to: new Twilio.Types.PhoneNumber(toNumber)
-            );
-
-            Console.WriteLine(message.Sid);
         }
     }
 }
