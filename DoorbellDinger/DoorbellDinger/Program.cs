48f3d68af4328ad7ef3b5d4cf76fe11bb8eb18b0
ChaoticEvan <evan.voordeckers@yahoo.com>
2022-01-23 12:46:43 -0700

    Added Twilio Nuget and user secrets to store phone numbers, account id, and auth token

--- DoorbellDinger/DoorbellDinger/Program.cs
+++ DoorbellDinger/DoorbellDinger/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
 namespace DoorbellDinger
 {
@@ -6,7 +8,20 @@ namespace DoorbellDinger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Find your Account SID and Auth Token at twilio.com/console
+            // and set the environment variables. See http://twil.io/secure
+            string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "Does this work?",
+                from: new Twilio.Types.PhoneNumber("+15017122661"),
+                to: new Twilio.Types.PhoneNumber("+15558675310")
+            );
+
+            Console.WriteLine(message.Sid);
         }
     }
 }
